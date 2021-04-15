ed5d5f81ee23876b0ffeb16fc39910cbb2de1b20
jonz94 <jody16888@gmail.com>
2021-04-15 08:19:46 +0800

    😎 feat: make pages of vite's dev server cross-origin isolated

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,18 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'cross-origin-isolated',
+      apply: 'serve',
+      configureServer({ middlewares }) {
+        middlewares.use((_, res, next) => {
+          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+          next();
+        });
+      },
+    },
+  ],
 });
